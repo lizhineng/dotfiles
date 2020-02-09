1f06966809ddba01496a230509e1806702b7007d
Li Zhineng <im@zhineng.li>
2020-02-09 19:27:38 +0800

    Extract to Brewfile

---
+++ Brewfile
@@ -0,0 +1,53 @@
+# Taps
+tap "homebrew/cask"
+
+# Better alternatives
+# https://remysharp.com/2018/08/23/cli-improved
+brew "bat" # > cat
+brew "diff-so-fancy" # > diff
+brew "fd" # > find
+brew "fzf" # > ctrl+r
+brew "htop" # > top
+brew "ncdu" # > du
+brew "prettyping" # > ping
+brew "tldr" # > man, alias to `help`
+
+# Binaries
+brew "composer"
+brew "git"
+brew "gnupg"
+brew "imagemagick"
+brew "mysql"
+brew "nginx"
+brew "node"
+brew "php"
+brew "pinentry-mac"
+brew "pkg-config"
+brew "redis"
+brew "tree"
+brew "vim"
+
+# General
+cask "1password"
+cask "alfread"
+cask "discord"
+cask "firefox-developer-edition"
+cask "google-chrome-canary"
+cask "google-drive-file-stream"
+cask "telegram"
+cask "tor-browser"
+cask "tweetbot"
+
+# Development
+cask "android-platform-tools"
+cask "docker"
+cask "github"
+cask "iterm2"
+cask "java"
+cask "phpstorm"
+cask "sublime-text"
+cask "tableplus"
+
+# Design Tools
+cask "adobe-creative-cloud"
+cask "figma"
--- setup.sh
+++ setup.sh
@@ -1,38 +1,13 @@
 # Install Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew update && brew upgrade
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew bundle
 
 # Install command line tools
 xcode-select --install
-
-brew install gnupg
-brew install pinentry-mac
-brew install ruby
-brew install vim
-brew install redis
-
-
-#
-# Node Development
-#
-
-brew install node
-
-
-#
-# PHP Development
-#
-
-brew install php
-brew install mysql
-
-# Install Composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer
 
 # Install Laravel packages
 composer global require laravel/installer
@@ -40,41 +15,6 @@ composer global require laravel/valet
 valet install
 
 
-#
-# Deisgn
-#
-
-brew cask install adobe-creative-cloud
-brew cask install figma
-
-#
-# Useful cli improved
-#
-
-brew install bat
-brew install prettyping
-brew install fzf
 $(brew --prefix)/opt/fzf/install
-brew install htop
-brew install diff-so-fancy
-brew install tldr
-brew install fd
-brew install ncdu
-
-
-#
-# GUI applications
-#
-
-brew cask install github
-brew cask install iterm2
-brew cask install sublime-text
-brew cask install 1password
-brew cask install tableplus
-brew cask install github-desktop
-brew cask isntall google-chrome
-brew cask install spotify
-brew cask install telegram
-brew cask install discord
 
 brew cleanup
