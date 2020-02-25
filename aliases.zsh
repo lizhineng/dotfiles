c547c48c8ea9ac7b0b876ea4012a7fd488572198
Li Zhineng <im@zhineng.li>
2020-02-25 15:18:24 +0800

    Extract to dedicated files

---
+++ aliases.zsh
@@ -0,0 +1,56 @@
+# Shortcuts
+alias c="clear"
+alias v="vim"
+alias reloadcli="source $HOME/.zshrc"
+alias reloaddns="dscacheutil -flushcache && killall -HUP mDNSResponder"
+
+# Directories
+alias dotfiles="cd $HOME/code/dotfiles"
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+
+# Keep system up-to-date
+# Apply updates for macOS, brew, composer and npm and their installed packages.
+alias update="sudo softwareupdate -ia; brew update; brew upgrade; brew cleanup; composer self-update; composer global update; npm install npm -g; npm update -g"
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Copy SSH public key to clipboard
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+
+# Better alternatives
+# https://remysharp.com/2018/08/23/cli-improved
+alias cat="bat"
+alias ping="prettyping --nolegend"
+alias preview="fzf --preview 'bat --color always {}'"
+alias top="sudo htop"
+alias help="tldr"
+alias du="ncdu --color dark -rr -x --exclude .git --exclude node_modules --exclude vendor"
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Show/hide all desktop icons
+alias cleandesk="defaults write com.apple.finder CreateDesktop false; killall Finder"
+alias messydesk="defaults write com.apple.finder CreateDesktop true; killall Finder"
+
+# URL-encode strings
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
+
+# Laravel
+alias a="php artisan"
+alias ams="php artisan migrate:fresh --seed"
+
+# PHPUnit
+alias p="./vendor/bin/phpunit"
+alias pf="./vendor/bin/phpunit --filter"
+
+# Javascript
+alias watch="npm run watch"
---
+++ path.zsh
@@ -0,0 +1,5 @@
+# Dependency manager for PHP
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+
+# Dependency management tool for Python
+export PATH="$HOME/.poetry/bin:$PATH"
