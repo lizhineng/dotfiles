9151f31f824349c3ffedd9bfe7c86755a72cf4bd
Li Zhineng <im@zhineng.li>
2023-08-13 09:00:33 +0800

    update telescope keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,7 +3,10 @@ return {
   tag = '0.1.2',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<cr>" },
-    { "<leader>gg", "<cmd>Telescope live_grep<cr>" }
-  }
+    { "<leader>f", [[<cmd>lua require('telescope.builtin').find_files()<cr>]] },
+    { "<leader>F", [[<cmd>lua require('telescope.builtin').find_files({ no_ignore = true, prompt_title = "All Files" })<cr>]] },
+    { "<leader>b", [[<cmd>lua require('telescope.builtin').buffers()<cr>]] },
+    { "<leader>h", [[<cmd>lua require('telescope.builtin').oldfiles()<cr>]] },
+    { "<leader>s", [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>]] },
+  },
 }
