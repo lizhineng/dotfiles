2ccfba2a02c5ee5b765b9074a7e2448fbb23a4d1
Li Zhineng <im@zhineng.li>
2024-02-23 11:02:38 +0800

    aliases t for testing

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -51,6 +51,7 @@ alias path='echo -e ${PATH//:/\\n}'
 # PHP
 # Force to reinstall all vendor packages completely
 alias cfresh="[[ -e composer.json ]] && rm -rf vendor/ composer.lock && composer i"
+alias t="composer test"
 
 # Laravel
 alias a="php artisan"
