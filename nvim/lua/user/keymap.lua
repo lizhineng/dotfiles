0dd8eaea15d33987a6a98b5e9a448300344606fd
Li Zhineng <im@zhineng.li>
2024-04-06 11:50:23 +0800

    register keymap with helper function

--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -1,19 +1,31 @@
+--- Adds a new keymap
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+-- Redefine the leader key
 vim.g.mapleader = ','
 
 -- Make it easy to edit VIM config file
-vim.keymap.set('n', '<Leader>ev', ':tabedit $MYVIMRC<cr>')
+map('n', '<Leader>ev', ':tabedit $MYVIMRC<cr>')
 
 -- Toggle the display of line number
-vim.keymap.set('n', '<c-n>', ':set number!<cr>')
+map('n', '<c-n>', ':set number!<cr>')
 
 -- Easy switching windows
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+map('n', '<C-j>', '<C-w><C-j>')
+map('n', '<C-k>', '<C-w><C-k>')
+map('n', '<C-h>', '<C-w><C-h>')
+map('n', '<C-l>', '<C-w><C-l>')
 
 -- Quick switch between buffers
-vim.keymap.set('n', '[b', ':bprevious<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', ']b', ':bnext<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', '[B', ':bfirst<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', ']B', ':blast<cr>', { silent = true, noremap = true })
+map('n', '[b', ':bprevious<cr>')
+map('n', ']b', ':bnext<cr>')
+map('n', '[B', ':bfirst<cr>')
+map('n', ']B', ':blast<cr>')
