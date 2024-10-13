3cb022e4d0fd9f77bee085e31b267bcbc2d60acd
Li Zhineng <im@zhineng.li>
2024-10-13 16:00:20 +0800

    link sublime text config directory

--- setup.sh
+++ setup.sh
@@ -40,3 +40,7 @@ brew cleanup
 if [ ! -d "$HOME/.config/karabiner" ]; then
     ln -s "$DOTFILES/karabiner" "$HOME/.config/karabiner"
 fi
+
+if [ ! -d "$HOME/Library/Application\ Support/Sublime\ Text/Packages/User" ]; then
+    ln -s "$DOTFILES/sublime" "~/Library/Application\ Support/Sublime\ Text/Packages/User"
+fi
